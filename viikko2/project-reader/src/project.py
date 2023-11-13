0525177a1f7bde50527f768b85856f3fb4f21bc3
Jimi Hiltunen <119453927+ValioEilax@users.noreply.github.com>
2023-11-13 23:29:01 +0200

    Add tehtävät 1-2

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -6,12 +6,16 @@ class Project:
         self.dev_dependencies = dev_dependencies
 
     def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+        return "\n- ".join(dependencies) if len(dependencies) > 0 else "-"
 
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\n"
+            f"\nDependencies:"
+            f"\n- {self._stringify_dependencies(self.dependencies)}"
+            f"\n"
+            f"\nDevelopment dependencies:"
+            f"\n- {self._stringify_dependencies(self.dev_dependencies)}"
         )
